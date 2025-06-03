0edef8ec83d120a1b04ac765f1a523dc1789a61c
Yurii Vydai <yuriivydai@gmail.com>
2025-06-03 12:10:55 +0300

    security improvements

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { AuthResolver } from './auth.resolver';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '600s' },
     }),
   ],
   providers: [
